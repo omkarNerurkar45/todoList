0928fd2ce2216c9533a425959018c1d83a2e08d7
Omkar Nerurkar <omkarnerurkar7@gmail.com>
2025-10-13 13:40:30 +0530

    Displaying todo list with help of Handlers

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,9 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className="bg-linear-to-r from-gray-800 via-blue-700 to-gray-900 text-amber-50 h-100vh pt-15">
-        <div className="todos border w-2/3 m-auto rounded-2xl h-[60vh] pt-2">
-          <h1 className="font-bold p-2 text-center">Add a Todo</h1>
+      <div className=" pb-20 h-[93.7vh] bg-linear-to-r from-gray-800 via-blue-700 to-gray-900 text-amber-50 h-100vh pt-15">
+        <div className="todos shadow-[20px_20px_50px_rgba(17,24,39,0.7)] border w-2/3 m-auto rounded-2xl h-[60vh] pt-2">
+          <h1 className="font-bold p-2 text-center text-2xl">Add a Todo</h1>
 
           <div className="todo text-center">
             <input
@@ -37,6 +37,19 @@ function App() {
               Add
             </button>
           </div>
+          <div className="list text-center font-bold pt-5 text-2xl">
+            Your Todos
+          </div>
+          {todos.map(item=>{
+
+          return <div className="display mx-auto m-2 text-amber-50 flex w-1/2  justify-between border-b-1 p-3">
+        <div className={item.isCompleted?"line-through":""} >{item.todo}</div>
+        <div className="buttons">
+        <button className="mx-1 border-1 w-20 rounded-3xl">Edit</button>
+        <button className="mx-1 border-1 w-20 rounded-3xl">Delete</button>
+        </div>
+      </div>
+      })}
         </div>
       </div>
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,19 +4,10 @@ const Navbar = () => {
   return (
     <div>
       <div>
-        <ul className="flex justify-around text-md pt-9 bg-linear-to-r from-gray-800 via-blue-700 to-gray-900 text-amber-50">
+        <ul className="flex justify-around text-1 pt-9 bg-linear-to-r from-gray-800 via-blue-700 to-gray-900 text-amber-50">
           <li className="cursor-pointer hover:font-bold">Home</li>
           <li className="cursor-pointer hover:font-bold">Your Tasks</li>
           <li className="cursor-pointer hover:font-bold">About</li>
-          <li className=" flex  pr-10">
-          <div className="cursor-pointer w-11 mx-2 h-6  bg-gray-300 peer-focus:ring-2 peer-focus:ring-indigo-400
-                      rounded-full peer-checked:bg-indigo-600 transition-colors duration-200 relative">
-        <span
-          className="absolute left-0.5 top-0.5 w-5 h-5 bg-white rounded-full shadow
-                     transition-transform duration-200 peer-checked:translate-x-5"
-        ></span>
-      </div>Enable Dark Mode
-          </li>
         </ul>
       </div>
     </div>
